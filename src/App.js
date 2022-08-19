ea882d5f93ef554739be413eaab97e340c61dd82
GeorgZippa <111534204+GeorgZippa@users.noreply.github.com>
2022-08-19 17:31:31 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const form = () => {
+    return(
+    <div>
+      <input />
+    </div>
+  );
+};
+
+const form = () => {
+  const [inputValue, setInputValue] = useState (initialState: '9900')
+  return(
+    <div>
+      <input value={inputValue} />
+    </div>
+  );
+};
